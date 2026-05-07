985f086a79656ce745d2399853f0882189d6a05f
AnhXuan04 <22130333@st.hcmuaf.edu.vn>
2026-05-08 03:38:31 +0700

    bổ sung 2 file thiếu liên quan  khi tách bảng db

---
+++ core-service/src/main/java/com/example/demo/repository/OtpVerificationRepository.java
@@ -0,0 +1,15 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.OtpVerification;
+import com.example.demo.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface OtpVerificationRepository extends JpaRepository<OtpVerification, Long> {
+    Optional<OtpVerification> findByUserAndOtpCode(User user, String otpCode);
+    Optional<OtpVerification> findFirstByUserOrderByCreatedAtDesc(User user);
+}
+
